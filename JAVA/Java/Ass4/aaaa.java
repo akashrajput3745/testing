d269532483df8bb025781b40e29ca66a2376d5f7
akashrajput3745 <akashrajput3745@gmail.com>
2025-10-12 21:30:36 +0530

    last chang

---
+++ JAVA/Java/Ass4/aaaa.java
@@ -0,0 +1,8 @@
+import testing.Info;
+
+public class aaaa {
+    public static void main(String args[]) {
+        Info s = new Info();
+        s.display();
+    }
+}
---
+++ JAVA/Java/Ass4/testing/Info.java
@@ -0,0 +1,7 @@
+package testing;
+
+public class Info {
+    public void display() {
+        System.out.println("Student Information");
+    }
+}
